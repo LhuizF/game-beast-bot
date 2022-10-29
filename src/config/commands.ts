0016d81744c61e4cc587c3fc16f135f6ff7a50e7
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-10-29 19:31:02 -0300

    fix: correct path for commands folder in build

--- src/config/commands.ts
+++ src/config/commands.ts
@@ -1,9 +1,12 @@
 import fs from 'fs';
+import path from 'path';
 import { Command } from '../types/protocols/command';
 
 export const useCommands = async () => {
+  const commandsFolder = path.join(__dirname, '../', './commands');
+
   const commands: Array<[string, Command]> = await Promise.all(
-    fs.readdirSync('./src/commands').map(async (file) => {
+    fs.readdirSync(commandsFolder).map(async (file) => {
       const command = (await import(`../commands/${file}`)).default;
       console.log(`/${command.name}`);
       return [command.name, { ...command, handle: command.handle }];
