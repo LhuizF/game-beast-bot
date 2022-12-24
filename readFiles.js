27ac1cc79ad3537b6d39833c5f3d321902d44cb9
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-12-24 17:57:28 -0300

    feat: add script read files

---
+++ readFiles.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+
+function getPath(basePath, search, foundFiles) {
+  fs.readdirSync(basePath).map(async (file) => {
+    const pathFile = `${basePath}/${file}`;
+    const stat = fs.statSync(pathFile);
+
+    if (stat.isDirectory()) {
+      getPath(pathFile, search, foundFiles);
+    } else {
+      readFile(pathFile, search, foundFiles);
+    }
+  });
+}
+
+function readFile(path, search, foundFiles) {
+  const file = fs.readFileSync(path, 'utf8');
+  if (file.includes(search)) {
+    const lines = file.split('\n');
+    const index = lines.findIndex((line) => line.includes(search)) + 1;
+    foundFiles.push({ path, index });
+  }
+}
+
+function start() {
+  const basePath = './src';
+  const searches = ['isLocal: true'];
+
+  searches.forEach((search) => {
+    const foundFiles = [];
+    getPath(basePath, search, foundFiles);
+
+    if (foundFiles.length > 0) {
+      console.log(foundFiles);
+      const err = `Arquivos encontrados com: ${search}`;
+      throw new Error(err);
+    }
+  });
+}
+
+start();
